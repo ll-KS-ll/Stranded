b86fa3b106fc3f10049709d055032fd99ab31edf
K-S <karlssonsehlin@gmail.com>
2013-02-23 01:28:20 +0100

    New splash screen

--- PA/gen/com/brogames/bro/R.java
+++ PA/gen/com/brogames/bro/R.java
@@ -10,6 +10,9 @@ package com.brogames.bro;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int stranded_color=0x7f050000;
+    }
     public static final class drawable {
         public static final int bag=0x7f020000;
         public static final int bag_layout=0x7f020001;
@@ -22,14 +25,16 @@ public final class R {
         public static final int ic_launcher=0x7f020008;
         public static final int items=0x7f020009;
         public static final int player_layout=0x7f02000a;
-        public static final int splash_screen=0x7f02000b;
-        public static final int ui=0x7f02000c;
+        public static final int splash=0x7f02000b;
+        public static final int splash_screen=0x7f02000c;
+        public static final int ui=0x7f02000d;
     }
     public static final class id {
-        public static final int bExit=0x7f060003;
-        public static final int bOptions=0x7f060002;
-        public static final int bScore=0x7f060001;
-        public static final int bStart=0x7f060000;
+        public static final int bExit=0x7f070004;
+        public static final int bOptions=0x7f070003;
+        public static final int bScore=0x7f070002;
+        public static final int bStart=0x7f070001;
+        public static final int imageView1=0x7f070000;
     }
     public static final class layout {
         public static final int splash_screen=0x7f030000;
@@ -37,6 +42,7 @@ public final class R {
     }
     public static final class string {
         public static final int app_name=0x7f040000;
+        public static final int developer_team=0x7f040008;
         public static final int exit=0x7f040003;
         public static final int imageview_descript=0x7f040007;
         public static final int menu_settings=0x7f040001;
@@ -46,6 +52,6 @@ public final class R {
         public static final int title_activity_lunch=0x7f040002;
     }
     public static final class style {
-        public static final int AppTheme=0x7f050000;
+        public static final int AppTheme=0x7f060000;
     }
 }
